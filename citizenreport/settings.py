ba5cf07a0f6686e07958b41e690d785a7e57a49b
Dev <devkreat@famcorpp.com>
2023-07-27 20:31:48 +0200

    updated engine

--- citizenreport/settings.py
+++ citizenreport/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'citizenreport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'reportdb',
         'USER': 'Eniola',
         'PASSWORD':'London2020*',
