9220966c5b43e4df0980825f161a67794f0501e4
devkreat <69965296+devkreat@users.noreply.github.com>
2023-07-27 20:41:33 +0200

    Update settings.py

--- citizenreport/settings.py
+++ citizenreport/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'citizenreport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'reportdb',
         'USER': 'Eniola',
         'PASSWORD':'London2020*',
